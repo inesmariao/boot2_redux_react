2d365bcef4a3ae16524819cdb5c56d8333f4852b
inesmariao <inesmao.misena@gmail.com>
2024-07-18 07:02:33 -0500

    complete

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,6 +1,22 @@
+import { useDispatch, useSelector } from "react-redux";
+import { setName } from '../redux/slice';
 
 export const User = () => {
+
+  const name = useSelector((state) => state.user.name);
+  const dispatch = useDispatch();
+
+  const handleSetName = () => {
+    const newName = prompt("Digita tu nombre: ");
+    dispatch(setName(newName));
+  }
+
   return (
-    <div>User</div>
+    <div>
+      <h1>User: {name} </h1>
+      <div className="my-4">
+        <button onClick={handleSetName} >Asignar nombre</button>
+      </div>
+    </div>
   )
 }
